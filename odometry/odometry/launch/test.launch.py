c642086c37ce4f60f245abeaaa329c02c8ee88c4
Andrew Philip Thombra <70837354+andrewthombra20@users.noreply.github.com>
2024-03-22 13:28:27 +0100

    Update test.launch.py

--- odometry/odometry/launch/test.launch.py
+++ odometry/odometry/launch/test.launch.py
@@ -1,36 +1,4 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node, ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
-
-def generate_launch_description():
-    """Generate launch description with multiple components."""
-
-    container = ComposableNodeContainer(
-        name="commands_container",
-        namespace="",
-        package="rclcpp_components",
-        executable="component_container",
-        composable_node_descriptions=[
-            ComposableNode(
-                package="odometry",
-                plugin="Odometry",
-                name="odometry_node",
-                parameters=[{
-                    "wheel_circumference": 1.158, # in meters
-                    "compensation_factor": 0.035,
-                    "gear_ratio": 58.0,
-                    "encoder_ppr": 1024,
-                    "track_width": 0.6,
-                    }],
-                extra_arguments=[{"use_intra_process_comms": True}],
-            ),
-        ],
-    )
-
-    return LaunchDescription([container])
-
-
-from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import ComposableNodeContainer, Node
@@ -111,24 +79,18 @@ def generate_launch_description():
                 parameters=[{"frequency": 10.0}],
                 extra_arguments=[{"use_intra_process_comms": True}],
             ),
-            container = ComposableNodeContainer(
-            name="commands_container",
-            namespace="",
-            package="rclcpp_components",
-            executable="component_container",
-            composable_node_descriptions=[
-                ComposableNode(
-                    package="odometry",
-                    plugin="Odometry",
-                    name="odometry_node",
-                    parameters=[{
-                        "wheel_circumference": 1.158, # in meters
-                        "compensation_factor": 0.035,
-                        "gear_ratio": 58.0,
-                        "encoder_ppr": 1024,
-                        "track_width": 0.6,
-                        }],
-                    extra_arguments=[{"use_intra_process_comms": True}],
+            ComposableNode(
+                package="odometry",
+                plugin="Odometry",
+                name="odometry_node",
+                parameters=[{
+                    "wheel_circumference": 1.158, # in meters
+                    "compensation_factor": 0.035,
+                    "gear_ratio": 58.0,
+                    "encoder_ppr": 1024,
+                    "track_width": 0.6,
+                    }],
+                extra_arguments=[{"use_intra_process_comms": True}],
             ),
         ],
         output="screen",
